30c1204b02e8a658f9c3cbc257f76a8465a950a2
Michele <michele@MacBook-Pro-di-Michele.local>
2019-02-23 13:59:26 +0100

    ora importa le dash

--- src/components/config.js
+++ src/components/config.js
@@ -1,128 +1,37 @@
+import _ from 'lodash';
+import config from 'grafana/app/core/config';
 import {getJsonAsText, path} from '../utils/utils.js';
 import locationUtil from '../utils/location_util';
-
-export class DreamCorpAppConfigCtrl{
+export class DreamCorpAppConfigCtrl {
 
   /** @ngInject */
-  constructor($scope,$injector,$location,backendSrv) {
-    this.$location = $location;
+  constructor(backendSrv, validationSrv, navModelSrv, $location, $routeParams) {
+    //this.navModel = navModelSrv.getNav('create', 'import');
     this.backendSrv = backendSrv;
-    console.info("App instance");
-    const my_url = path()+'/networks/rete.json';
-    getJsonAsText(this,my_url);
-    //this.startLoop();
-  }
+    this.validationSrv = validationSrv;
+    this.$location = $location;
+    this.$routeParams = $routeParams;
+    this.step = 1;
+    this.nameExists = false;
+    this.uidExists = false;
+    this.autoGenerateUid = true;
+    this.autoGenerateUidValue = 'auto-generated';
+    this.folderId =  $routeParams.folderId ? Number($routeParams.folderId) || 0 : null;
+    this.initialFolderTitle = 'Select a folder';
 
-  startLoop(){
-    //test loop
-    var i = 0;
-    const myVar = setInterval(function(){
-      console.info(i);
-      i++;
-      if(i===20)
-        setTimeout(function (){
-          clearInterval(myVar)
-        },3000);
-    }, 1000);
-
-
-  }
-
-  createBN(text) {
-    console.info("createBN()");
-    var rete = JSON.parse(text);
-
-    const jsbayes = require('jsbayes');
-    this.g = jsbayes.newGraph();
-
-    //function to set the variables
-    this.onInitData();
-
-    this.nets.push(rete.rete); //per ora un solo valore
-
-    let i;
-    //catturo le informazioni e creo i nodi
-    for (i = 0; i<rete.nodi.length; i++){ //per tutti i nodi li creo e li metto i una lista
-      this.id_nodes.push(rete.nodi[i].id); //mi salvo l'id del nodo
-      this.states_nodes.push(rete.nodi[i].stati); //mi salvo gli stati
-      this.threshold_nodes.push(rete.nodi[i].soglie); //mi salvo le soglie
-      this.nodi.push(this.g.addNode(this.id_nodes[i],this.states_nodes[i])); //nuovo nodo logico della rete bayesiana
-      //this.nodi[i].setCpt(rete.nodi[i].cpt); //inserisco le cpt dentro ai nodi e non più random
+    // check gnetId in url
+    if ($routeParams.gnetId) {
+      this.gnetUrl = $routeParams.gnetId;
+      this.checkGnetDashboard();
     }
-
-    this.nodes.push(this.id_nodes); //inserisco un array dentro un altro per essere pronto a gestire più reti
-
-    var index, parent_name;
-
-    //creo le relazioni padre-figlio
-    for (i = 0; i<rete.nodi.length; i++){ //per tutti i nodi aggiungo i genitori
-      if(rete.nodi[i].parents !== null) { // se ha almeno un padre
-        for (let j = 0; j < rete.nodi[i].parents.length; j++) {
-
-          parent_name = rete.nodi[i].parents[j]; //mi salvo il nome del parent
-
-          for(let k =0;k<rete.nodi.length;k++){ //per tutti i nodi cerco il parent con quel nome specifico
-            if(parent_name === this.nodi[k].name) index = k;
-          }
-          //devo passare la variabile stessa non la stringa
-          this.nodi[i].addParent(this.nodi[index]);
-        }
-      }
-    }
-    //random cpt
-    this.g.reinit()
-        .then(function(r) {
-          //return this.g.sample(10000); //likelihood weight sampling aka the inference
-        });
-
   }
-  onInitData(){
-    console.info("onInitData()");
-    //utilizzo degli array contenenti varie informazioni sulla rete che andrò a sfruttare durante l'esecuzione del programma
-    this.nodi = []; //array di variabili di nodi logici di jsbayes (qui dentro inserisco quello che ritorna la funzione g.addNode(nome, stati))
 
-    //variabili per i valori di presentazione a schermo dell'informazione
-    this.nets = []; //nomi delle reti
-    this.id_nodes = []; //id dei nodi
-    this.nodes = []; //insieme dei nomi dei nodi di una rete
-    this.states_nodes = []; //stati dei nodi
-    this.threshold_nodes = []; //soglie dei nodi
-    this.samples = 10000; //numero di sampling da fare, il default, utilizzato anche da jsbayes, è 10k
-
-
-    //variabili che memorizzano la posizione all'interno degli array precedenti che è stata scelta dall'utente a schermo
-    this.netPos = null; //indice della rete scelta
-    this.nodePos = null; //indice del nodo scelto
-    this.statePos = null; //indice dello stato scelto
-    this.thresholdPos = null; //indice della soglia scelta
-
-    /*
-    //variabili grafiche che memorizzano il valore scelto dall'utente a schermo
-    this.panel.rete_id = null; //nome della rete scelta
-    this.panel.node_id = null; //nome del nodo scelto
-    this.panel.states_node_id = null; //nome dello stato scelto
-    this.panel.threshold_node_id = null; //valore della soglia scelta
-    this.panel.samples = 10000; //numero di sample scelto
-    */
-    //ricorda se è già stato associato un nodo oppure no
-    this.associated = false;  //true: associato / false: non associato
-  }
-  showProb(){
-    //output probabilità
-    for (let x = 0;x<this.nodi.length;x++){
-      console.info(this.nodi[x].probs());
-    }
-
-  }
   onUpload(dash) {
-    //this.createBN(dash);
-    //console.log(this.g);
-    this.dash = 0;
     this.dash = dash;
-    //this.dash.id = null;
-    //this.step = 2;
-    //this.inputs = [];
-    /*
+    this.dash.id = null;
+    this.step = 2;
+    this.inputs = [];
+
     if (this.dash.__inputs) {
       for (const input of this.dash.__inputs) {
         const inputModel = {
@@ -143,18 +52,125 @@ export class DreamCorpAppConfigCtrl{
 
         this.inputs.push(inputModel);
       }
-    }*/
+    }
 
+    this.inputsValid = this.inputs.length === 0;
+    this.titleChanged();
+    this.uidChanged(true);
+  }
+
+  setDatasourceOptions(input, inputModel) {
+    const sources = _.filter(config.datasources, val => {
+      return val.type === input.pluginId;
+    });
+
+    if (sources.length === 0) {
+      inputModel.info = 'No data sources of type ' + input.pluginName + ' found';
+    } else if (!inputModel.info) {
+      inputModel.info = 'Select a ' + input.pluginName + ' data source';
+    }
+
+    inputModel.options = sources.map(val => {
+      return { text: val.name, value: val.name };
+    });
+  }
+
+  inputValueChanged() {
+    this.inputsValid = true;
+    for (const input of this.inputs) {
+      if (!input.value) {
+        this.inputsValid = false;
+      }
+    }
+  }
+
+  titleChanged() {
+    this.titleTouched = true;
+    this.nameExists = false;
+
+    this.validationSrv
+        .validateNewDashboardName(this.folderId, this.dash.title)
+        .then(() => {
+          this.nameExists = false;
+          this.hasNameValidationError = false;
+        })
+        .catch(err => {
+          if (err.type === 'EXISTING') {
+            this.nameExists = true;
+          }
+
+          this.hasNameValidationError = true;
+          this.nameValidationError = err.message;
+        });
+  }
+
+  uidChanged(initial) {
+    this.uidExists = false;
+    this.hasUidValidationError = false;
+
+    if (initial === true && this.dash.uid) {
+      this.autoGenerateUidValue = 'value set';
+    }
+
+    this.backendSrv
+        .getDashboardByUid(this.dash.uid)
+        .then(res => {
+          this.uidExists = true;
+          this.hasUidValidationError = true;
+          this.uidValidationError = `Dashboard named '${res.dashboard.title}' in folder '${
+              res.meta.folderTitle
+              }' has the same uid`;
+        })
+        .catch(err => {
+          err.isHandled = true;
+        });
+  }
+
+  onFolderChange(folder) {
+    this.folderId = folder.id;
+    this.titleChanged();
+  }
+
+  onEnterFolderCreation() {
+    this.inputsValid = false;
+  }
+
+  onExitFolderCreation() {
+    this.inputValueChanged();
+  }
+
+  isValid() {
+    return this.inputsValid && this.folderId !== null;
+  }
+
+  saveDashboard() {
+    const inputs = this.inputs.map(input => {
+      return {
+        name: input.name,
+        type: input.type,
+        pluginId: input.pluginId,
+        value: input.value,
+      };
+    });
+
+    return this.backendSrv
+        .post('api/dashboards/import', {
+          dashboard: this.dash,
+          overwrite: true,
+          inputs: inputs,
+          folderId: this.folderId,
+        })
+        .then(res => {
+          const dashUrl = locationUtil.stripBaseFromUrl(res.importedUrl);
+          this.$location.url(dashUrl);
+        });
   }
 
   loadJsonText() {
     try {
       this.parseError = '';
-      this.createBN(this.jsonText);
-      //console.log(this.g);
       const dash = JSON.parse(this.jsonText);
       this.onUpload(dash);
-      this.saveDashboard();
     } catch (err) {
       console.log(err);
       this.parseError = err.message;
@@ -162,20 +178,41 @@ export class DreamCorpAppConfigCtrl{
     }
   }
 
-  saveDashboard() {
-    console.log("fun chiamata")
+  checkGnetDashboard() {
+    this.gnetError = '';
+
+    const match = /(^\d+$)|dashboards\/(\d+)/.exec(this.gnetUrl);
+    let dashboardId;
+
+    if (match && match[1]) {
+      dashboardId = match[1];
+    } else if (match && match[2]) {
+      dashboardId = match[2];
+    } else {
+      this.gnetError = 'Could not find dashboard';
+    }
 
     return this.backendSrv
-        .post('api/dashboards/import', {
-          dashboard: this.dash,
-          overwrite: true
-        })
+        .get('api/gnet/dashboards/' + dashboardId)
         .then(res => {
-          const dashUrl = locationUtil.stripBaseFromUrl("http://localhost:3000/plugins/dreamcorp-app/page/add-bayesian-network");
-          this.$location.url(dashUrl);
-          console.log("rete salvata")
-;        });
+          this.gnetInfo = res;
+          // store reference to grafana.com
+          res.json.gnetId = res.id;
+          this.onUpload(res.json);
+        })
+        .catch(err => {
+          err.isHandled = true;
+          this.gnetError = err.data.message || err;
+        });
+  }
+
+  back() {
+    this.gnetUrl = '';
+    this.step = 1;
+    this.gnetError = '';
+    this.gnetInfo = '';
   }
 }
+
 
 DreamCorpAppConfigCtrl.templateUrl = 'components/config.html';
