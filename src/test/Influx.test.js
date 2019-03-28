b92b170dea2fe957b697705e7060210c312deded
Pietro Casotto <pietrocas8@gmail.com>
2019-03-28 10:27:19 +0100

    sistemazione test createDB

--- src/test/Influx.test.js
+++ src/test/Influx.test.js
@@ -1,16 +1,15 @@
-jest.mock('jquery');
 const $ = require('jquery');
-/*window.$ = {ajax: jest,genMockFunction()}*/
 const Influx = require("../utils/Influx");
+
+jest.mock('jquery');
 
 global.console = {
     warn: jest.fn(),
     log: jest.fn(),
-    info: jest.fn(),
-    ajax: jest.fn()
+    info: jest.fn()
 };
-//jest.resetModules();
-//console.info = jest.fn();
+
+console.info = jest.fn();
 
 let undefinedInflux = new Influx(null,null,null);
 
@@ -24,35 +23,12 @@ describe("Created null Influx class", () => {
     });
 });
 
-let query = 'q=CREATE DATABASE '+ undefinedInflux.database;
+//let query = 'q=CREATE DATABASE '+ undefinedInflux.database;
 
-/*undefinedInflux.createDB().mockImplementation(
-  cb  => {
-      cb({
-          url: undefinedInflux.host + '/query?',
-          type: 'GET',
-          contentType: 'application/octet-stream',
-          data: query,
-          processData: false,
-      });
-  })
-*/
 
-it("Create null CreateDB", async () => {
-    //expect.assertions(1);
+test("Called function createDB", ()=>{
     undefinedInflux.createDB().catch((err)=>console.log(err));
-    await expect($.ajax).toEqual({
-        url:null+null+'/query?',
-        type:'GET',
-        contentType:'application/octet-stream',
-        data: query,
-        processData: false,
-        error: function(test, status, exception) {
-            console.log("Error: " + exception);
-        }
-    });
+
+        afterEach(() => expect($.ajax()).toBeCalled()
+        )
 });
-
-
-
-
